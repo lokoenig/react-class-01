1f681d606cb963b155dd59c4bf9fba9e7af87e04
Koenig <lokoenig@deloitte.com>
2023-01-20 14:58:36 -0600

    figure out how react router 6 works.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,50 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
+
+
+const Dashboard = ()=> (
+  <div>
+    <h2>dashboard</h2>
+  </div>
+);
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "dash",
+    element: <Dashboard />
+  }
+]);
+
+
+
+
+
+const About = ()=> (
+  <p>
+    Enough about me. Let's talk about me.
+  </p>
+);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
