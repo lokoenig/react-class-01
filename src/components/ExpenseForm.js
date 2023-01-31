1072f6a63de80c26c8eb013b1edc7d4e09605101
Koenig <lokoenig@deloitte.com>
2023-01-31 15:51:29 -0600

    Add a date picker to expense form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,9 +1,25 @@
 import React from "react";
+import moment from "moment";
+
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import dayjs, { Dayjs } from 'dayjs';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+
+
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+
+
+
+
 
 export default class ExpenseForm extends React.Component {
     state = {
         description: '',
-        note: ''
+        note: '',
+        amount: '0',
+        created: moment()
     };
     onChangeDescription = (e) => {
         const newDiscription = e.target.value;
@@ -11,6 +27,7 @@ export default class ExpenseForm extends React.Component {
             description: newDiscription
         }));
     };
+
     onChangeNote = (e) => {
         const note = e.target.value;
         this.setState(() => ({
@@ -18,6 +35,22 @@ export default class ExpenseForm extends React.Component {
         }));
     };
 
+    onChangeAmount = (e)=>{
+        const v = e.target.value;
+        if (v.match(/^\d*\.?\d{0,2}$/)) {
+            this.setState(() => ({
+                amount: v
+            }));
+        }
+    };
+
+    onChangeDate = (newValue: Dayjs | null) => {
+        this.setState(() => ({
+            created: newValue
+        }));
+        console.log('changedate', this.state);
+
+    };
 
     render() {
         return (
@@ -32,8 +65,11 @@ export default class ExpenseForm extends React.Component {
                         autoFocus
                     />
                     <input
-                        type="number"
+                        type="text"
                         placeholder="Amount"
+                        value={this.state.amount}
+                        onChange={this.onChangeAmount}
+
                     />
                     <textarea
                         placeholder="Note"
@@ -41,6 +77,16 @@ export default class ExpenseForm extends React.Component {
                         onChange={this.onChangeNote}
                         >
                     </textarea>
+
+                    <LocalizationProvider dateAdapter={AdapterMoment}>
+                        <DatePicker
+                            label="Date Added"
+                            value={this.state.created}
+                            onChange={this.onChangeDate}
+                            renderInput={(params) => <TextField {...params} />}
+
+                        />
+                    </LocalizationProvider>
                     <button>Add/Update Expense</button>
                 </form>
             </>
