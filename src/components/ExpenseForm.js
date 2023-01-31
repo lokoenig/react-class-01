db50671b75d6ebf2fa35e04e99f95a3d62921b6f
Koenig <lokoenig@deloitte.com>
2023-01-31 10:22:01 -0600

    Add input fields and listeners to expense form.

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,48 @@
 import React from "react";
 
 export default class ExpenseForm extends React.Component {
+    state = {
+        description: '',
+        note: ''
+    };
+    onChangeDescription = (e) => {
+        const newDiscription = e.target.value;
+        this.setState(() => ({
+            description: newDiscription
+        }));
+    };
+    onChangeNote = (e) => {
+        const note = e.target.value;
+        this.setState(() => ({
+            note
+        }));
+    };
+
+
     render() {
         return (
             <>
                 <h1>I be a form!</h1>
+                <form>
+                    <input
+                        type="text"
+                        placeholder="Description"
+                        value={this.state.description}
+                        onChange={this.onChangeDescription}
+                        autoFocus
+                    />
+                    <input
+                        type="number"
+                        placeholder="Amount"
+                    />
+                    <textarea
+                        placeholder="Note"
+                        value={this.state.note}
+                        onChange={this.onChangeNote}
+                        >
+                    </textarea>
+                    <button>Add/Update Expense</button>
+                </form>
             </>
         )
     }
