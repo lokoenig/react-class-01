1ec8fe9fdad3c0c003b2f559af47f2fb7968409f
Koenig <lokoenig@deloitte.com>
2023-02-01 15:07:56 -0600

    enable redux dev tools.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -7,7 +7,9 @@ const configureStore = ()=> {
         combineReducers({
             expenses: expensesReducer,
             filters: filtersReducer
-        })
+        } ),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
     );
     return store;
 }
