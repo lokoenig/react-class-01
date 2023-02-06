f9582823b4d72169fadf3d97d49ef42e58ca93d3
Koenig <lokoenig@deloitte.com>
2023-02-06 15:38:45 -0600

    Start writing tests for selectors.

---
+++ src/selectors/expenses.test.js
@@ -0,0 +1,103 @@
+import uuid from 'react-uuid';
+import { startOfMonth, endOfMonth } from 'date-fns'
+import getFilteredExpenses from "./expenses";
+
+
+const ExpenseTestData = [
+    {
+    id: '737d0f8d-aab7-1508-25aa-8f0ea76e53fb',
+    description: 'booze',
+    note: 'Drinky drink',
+    amount: 14000,
+    created: (new Date(1995, 4,1))
+    },
+    {
+    id: 'ce66195c-6180-76e3-0797-9382b5b7a989',
+    description: 'rent',
+    note: 'Pay or move on',
+    amount: 120000,
+    created: (new Date(1999, 2, 27))
+    },
+    {
+    id: '17db2be4-02ff-9264-4f5d-c10f1cd80371',
+    description: 'Cat Supplies',
+    note: '',
+    amount: 3333,
+    created: (new Date(2002, 2, 27))
+    },
+    {
+    id: '5e5ca05d-9ccb-5476-d5f1-3bc7a4cb929a',
+    description: 'combs',
+    note: 'For all the hair!',
+    amount: 1666299,
+    created: (new Date(2018, 12, 20))
+    }
+];
+
+
+const FilterTestData = {
+    text: '',
+    sortBy: 'date', // date or amount
+    dateRange: {
+        start: (new Date(1980,0,0)),
+        end: (new Date(2029, 0, 0))
+    }
+}
+
+test('getFilteredExpenses (sort by date)', ()=>{
+    const filter = {
+        ...FilterTestData,
+        sortBy: 'date'
+    };
+    const expenses = [
+        ...ExpenseTestData,
+    ];
+    const result = getFilteredExpenses(ExpenseTestData, filter );
+    expect(result).toEqual([
+        expenses[3],
+        expenses[2],
+        expenses[1],
+        expenses[0]
+    ])
+});
+
+test('getFilteredExpenses (sort by amount)', () => {
+    const ff = {
+        ...FilterTestData,
+        sortBy: 'amount' // date or amount
+    };
+    
+    const expenses = [
+        ...ExpenseTestData,
+    ];
+
+    const result = getFilteredExpenses(ExpenseTestData, ff);
+    expect(result).toEqual([
+        expenses[3],
+        expenses[1],
+        expenses[0],
+        expenses[2]
+    ]);
+});
+
+
+test('getFilteredExpenses (filter date range only)', () => {
+    const filter = {
+        ...FilterTestData,
+        sortBy: 'date',
+        dateRange: {
+            start: (new Date(1999, 2, 2)),
+            end: (new Date(2011, 2, 2))
+        }
+       
+    };
+
+    const result = getFilteredExpenses(ExpenseTestData, filter);
+
+    expect(result).toEqual([
+        ExpenseTestData[3],
+        ExpenseTestData[2],
+        ExpenseTestData[1],
+        ExpenseTestData[0]
+    ])
+});
