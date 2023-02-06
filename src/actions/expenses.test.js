8a36f0cae06cc535502c0f0d9e2165198647be1e
Koenig <lokoenig@deloitte.com>
2023-02-06 13:08:19 -0600

    Add tests for Actions.

---
+++ src/actions/expenses.test.js
@@ -0,0 +1,57 @@
+import { addExpense, removeExpense, updateExpense } from './expenses';
+
+
+test('removeExpense', ()=>{
+    const result = removeExpense('ccTestID');
+    expect(result).toEqual({
+        type: 'REMOVE_EXPENSE',
+        expense: {
+            id: 'ccTestID'
+        }
+    });
+})
+
+
+test('updateExpense', ()=>{
+    const result = updateExpense('ccTestID', { id:'ccTestIDfalse', dummy:'test object'});
+    expect(result).toEqual({
+        type: 'UPDATE_EXPENSE',
+        id:'ccTestID',
+        updates: {
+            dummy: 'test object',
+            id: 'ccTestIDfalse'
+        }
+    })
+});
+
+test('addExpense populated', ()=>{
+    const expenseTestData = {
+        description: 'I am not a waffle __!!#',
+        amount: 66666,
+        created: new Date('6/15/2021'),
+        note: 'notes cannot contain waffles or egg products'
+    };
+
+    const result = addExpense(expenseTestData);
+    expect(result).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            ...expenseTestData,
+            id: expect.any(String)
+        }
+    })
+})
+
+test('addExpense defaults', ()=>{
+    const result = addExpense();
+    expect(result).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            description : '',
+            note : '',
+            amount : 0,
+            created: expect.anything(),
+            id: expect.any(String)
+        }
+    })
+});
