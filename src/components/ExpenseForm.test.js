63e89f7e1018080b56bc7b33276f7a73139960f3
Koenig <lokoenig@deloitte.com>
2023-02-09 14:33:14 -0600

    Create interactive tests for typing numbers on Expense form.

--- src/components/ExpenseForm.test.js
+++ src/components/ExpenseForm.test.js
@@ -9,43 +9,56 @@ import ExpenseForm from "./ExpenseForm";
 
 describe('ExpenseForm', () => {
     const onSubmit = jest.fn();
- 
+
     test('checks if empty ExpenseForm changed', () => {
         const targetExpense = Math.floor(Math.random() * ExpenseTestData.length);
 
         const { getByText, asFragment } = render(
             <ExpenseForm />
         );
-    
+
         expect(asFragment()).toMatchSnapshot();
     });
 
 
-test('submits empty ExpenseForm', ()=>{
-    const { getByText, asFragment } = render(
-        <ExpenseForm
-            onSubmit={onSubmit}
-         />
-    );
-    clickSubmitButton();
-    const alertElem = screen.getByRole('alert', {
-        name: /expense was not submitted/i
-    });
-    expect(alertElem).toBeInTheDocument(); // error message
-});
-
-    test('type', () => {
-        render(<textarea />)
-
-        user.type(screen.getByRole('textbox', {
-            name: /expense amount/i
+    test('submits empty ExpenseForm', () => {
+        const { getByText, asFragment } = render(
+            <ExpenseForm
+                onSubmit={onSubmit}
+            />
+        );
+        clickSubmitButton();
+        const alertElem = screen.getByRole('alert', {
+            name: /expense was not submitted/i
         });
-        expect(screen.getByRole('textbox')).toHaveValue('Hello,\nWorld!')
+        expect(alertElem).toBeInTheDocument(); // error message
+    });
+
+    test('type valid number', () => {
+        render(
+            <ExpenseForm
+                onSubmit={onSubmit}
+            />
+        );
+
+        const numberBox = screen.getByLabelText('Expense Amount');
+        user.type(numberBox, '{selectall}{backspace}123.2');
+        expect(numberBox).toHaveValue('123.2')
     })
 
+    test('type invalid number', () => {
+        render(
+            <ExpenseForm
+                onSubmit={onSubmit}
+            />
+        );
+        const numberBox = screen.getByLabelText('Expense Amount');
+        user.type(numberBox, '{selectall}{backspace}12z3.2rr555');
+        expect(numberBox).toHaveValue('123.25')
+    })
 });
 
 
 function clickSubmitButton() {
-    user.click(screen.getByRole('button', { name: /Add Expense/}));
+    user.click(screen.getByRole('button', { name: /Add Expense/ }));
 }
