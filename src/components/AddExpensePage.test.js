bf7fafb7d98be6a37e18b42c5afd409e1502ed23
Koenig <lokoenig@deloitte.com>
2023-02-09 17:07:58 -0600

    Test a snapshot of the AddExpensePage full page.

--- src/components/AddExpensePage.test.js
+++ src/components/AddExpensePage.test.js
@@ -1,2 +1,13 @@
 import React from "react";
-import AddExpensePage from "./AddExpensePage";
+import {AddExpensePage} from "./AddExpensePage";
+import { render, screen, waitFor, within } from '@testing-library/react';
+import user from '@testing-library/user-event';
+
+test('renders full Add Expense page', () => {
+    const subSpy = jest.fn();
+    const { getByText, asFragment } = render(
+        <AddExpensePage
+            onSubmit={subSpy}
+        />);
+    expect(asFragment()).toMatchSnapshot();
+});
