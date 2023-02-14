49ca3c861e306ac230d79f63d6e94036933cb9a2
Koenig <lokoenig@deloitte.com>
2023-02-14 14:41:28 -0600

    get rid of anonymous export

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from "react";
 import logo from '../logo.svg';
 
-export default ()=>(
+export const PageHeader = ()=>{(
     <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
     </header>
-);
+)};
+
+export default PageHeader;
