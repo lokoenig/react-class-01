55efa7ad5db772b2b847d2f4cb82d7e66242202f
Koenig <lokoenig@deloitte.com>
2023-02-16 17:27:55 -0600

    Add an alternate dummy expense for additional testing.

--- src/fixtures/expense-single.js
+++ src/fixtures/expense-single.js
@@ -1,7 +1,15 @@
-const ExpenseSingleTestData = {
+export const ExpenseSingleTestData = {
     description : 'A very special expense',
     note : 'This is a test note with §`weird cahrs.',
     amount : 66666,
     created : new Date()
 };
+
+export const ExpenseAlternateSingleTestData ={
+    description: 'A box of truffles of every type and flavor - an irresistible mixture of best-selling creamy, nutty, chewy and soft center favorites wrapped in See’s rich milk and dark chocolates',
+    note: 'Sometimes a note is just a note',
+    amount: '226.83',
+    created: new Date(2022,6,12)
+};
+
 export default ExpenseSingleTestData;
