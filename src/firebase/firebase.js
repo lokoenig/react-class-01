2893f00700d0c7f8e9421f918c62677f30a96d1a
Koenig <lokoenig@deloitte.com>
2023-02-15 18:11:54 -0600

    move credentials outside of git

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,20 +2,14 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set } from "firebase/database"
+import { getDatabase, ref, set } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyAM1st-rZwynhtvaZULRh4G7wBEwbURCTw",
-    authDomain: "letest-7fb83.firebaseapp.com",
-    databaseURL: "https://letest-7fb83-default-rtdb.firebaseio.com",
-    projectId: "letest-7fb83",
-    storageBucket: "letest-7fb83.appspot.com",
-    messagingSenderId: "31839172112",
-    appId: "1:31839172112:web:d054c389ff090715be7c95"
-};
+// moved the credentials out of git:
+import firebaseConfig from "../../credentials"
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -31,7 +25,7 @@ set(ref(database, 'user/id_' + 1 ), {
         state: 'MN'
     },
     attributes: {
-        height: '5\'9\"',
+        height: '5\'9"',
         weight: 350
     }
 });
