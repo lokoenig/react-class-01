09eb7d9938ea81087140615a8751767664b5137c
Koenig <lokoenig@deloitte.com>
2023-02-27 13:23:01 -0600

    Update <App> tests. Requires wiring up a mock store.

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from "react-router-dom";
+import configureStore from 'redux-mock-store' //ES6 modules
+import thunk from 'redux-thunk';
 
 import App from './App';
+import { Provider } from "react-redux";
+
+
+const mockStore = configureStore([thunk]);
+const store = mockStore({});
+
 
 test('App framework changes', () => {
     const { getByText, asFragment } = render(
+        <Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
+        </Provider>
         );
     //    screen.debug();
     expect(asFragment()).toMatchSnapshot();
--- src/components/LogOutLink.js
+++ src/components/LogOutLink.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from 'react-redux';
 
-import { startLogout } from "../actions/firebase-auth";
+import {startLogout} from "../actions/firebase-auth";
 
 const localLogOut = () => {
     startLogout();
