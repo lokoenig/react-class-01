298522ea6868e3b23b70f85091f2209d2ab0aa16
Koenig <lokoenig@deloitte.com>
2023-03-13 07:50:06 -0500

    reorganize scss as "theme." Add a logo.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { RouterProvider } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 // import my stuff:
-import './shared-styles/base.scss';
+import './theme/base.scss';
 import "./components/ExpenseForm.scss";
 import LoginPage from "./components/LoginPage";
 import { firebase } from './firebase/firebase';
