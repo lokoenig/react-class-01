2fbf4a88b1c79cfd74fb4e3c6a60c2af2ba92fce
Koenig <lokoenig@deloitte.com>
2023-03-14 22:26:10 -0500

    Start styling the expenses list layout.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,26 +1,31 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+
+
+
 import './ExpenseItem.scss';
 import EditExpenseButton from "./EditExpenseButton";
 
 // Description, amount, creation date
-const ExpenseItem = (props)=> (
-<div
-    className="expense-single"
-    key={props.id}
->
-    <span className="expense-single__description">{props.description}</span>
-    <span className="expense-single__amount">
+const ExpenseItem = (props) => (
+    <Link
+        to={'/edit/' + props.id}
+        className="expense-single"
+        key={props.id}
+    >
+        <span className="expense-single__description">{props.description}</span>
+        <span className="expense-singe__created">
+            {props.created.toLocaleDateString()}
+        </span>
+        <span className="expense-single__amount">
             {new Intl.NumberFormat(
                 'en-US',
                 { style: 'currency', currency: 'USD' }
             ).format(props.amount / 100)}
-    
-    </span>
-    <span className="expense-singe__created">
-    { props.created.toLocaleDateString()}
-    </span>
-        <EditExpenseButton expenseID={props.id} />
-</div>
+
+        </span>
+
+    </Link >
 );
 
 export default ExpenseItem;
--- src/components/ExpensesList.js
+++ src/components/ExpensesList.js
@@ -1,22 +1,31 @@
 import React from "react";
 import { connect } from "react-redux";
+
 import getFilteredExpenses from "../selectors/expenses";
 import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.scss";
+
 export const ExpensesList = (props) => {
     let template = (<div>No expenses in meet the search criteria.</div>);
     if (props.expenses.length !== 0) {
         template = (
-            <div>
-            
-            {
-                    props.expenses.map( (o, index) => 
-                    <ExpenseItem 
-                    key={index} 
-                    {...o}
-                    />
-                )
-            }
-            </div>
+            <><div className="expense-list">
+                <div className="expense-list__column-header">
+                    <span className="mobile-only expense-list__column-header--name">Expenses</span>
+                    <span className="desktop-only expense-list__column-header--name">Expense</span>
+                    <span className="desktop-only expense-list__column-header--amount">Amount</span>
+                </div>
+                <div className="expense-list__list">
+                {
+                    props.expenses.map((o, index) =>
+                        <ExpenseItem
+                            key={index}
+                            {...o}
+                        />
+                    )
+                }
+                </div>
+            </div></>
         );
     }
     return template;
